63cfc5bc83363296bd5980e099f175035d8c1868
kimmo <kimmo.ahola@systementor.se>
2026-05-13 14:47:43 +0200

    set headless to true

--- src/test/java/com/example/playwright_demo/BasePlaywrightTest.java
+++ src/test/java/com/example/playwright_demo/BasePlaywrightTest.java
@@ -14,7 +14,7 @@ public class BasePlaywrightTest {
         playwright = Playwright.create();
         browser = playwright.chromium().launch(
                 new BrowserType.LaunchOptions()
-                        .setHeadless(false)
+                        .setHeadless(true)
         );
     }
 
